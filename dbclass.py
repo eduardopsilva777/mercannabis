f9988bdf1e6a752118390f4e67a7277544b3b6f9
VictorGabriel8778 <vrg8778@gmail.com>
2026-09-14 21:31:54 -0300

    0.1

--- dbclass.py
+++ dbclass.py
@@ -1,11 +1,12 @@
 from db import *
 
 class produtos:
+    
     def cadastro (self, titulo, descricao, valor, cartegoria):
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
 
-        c.execute(""" INSERT INTO produtos (titulo, descricao, valor, cartegoria)
+        c.execute("""INSERT INTO produtos (titulo, descricao, valor, cartegoria)
         VALUES (?, ?, ?, ?)
         """, (titulo, descricao, valor, cartegoria))
 
@@ -16,19 +17,25 @@ class produtos:
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
 
-        c.execute(""" DELETE FROM produtos WHERE id = ?
+        c.execute("""DELETE FROM produtos WHERE id = ?
         """, (id))
 
         conn.commit()
         conn.close()
 
-    def editar (self, id, titulo, descricão, valor, cartegoria):
+    def editar (self, id, titulo, descricao, valor, cartegoria):
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
+        c.execute('UPDATE produtos SET titulo = ?, descricao = ?, valor = ?, cartegoria = ?,  WHERE id = ?', (titulo, descricao, valor, cartegoria)) 
+        conn.commit()
+        conn.close()
 
     def exibir (self):
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
+        c.execute("SELECT * FROM produtos")
+        conn.close()
+        return c.fetchall()
 
 class users:
     None
